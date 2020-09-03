c3e206a46101e33889b2b46c6e45fa44f2941f33
Alex Zhu <seazhui@gmail.com>
2020-09-02 22:49:59 -0700

    Update study_schudle.sh

--- study_schudle.sh
+++ study_schudle.sh
@@ -2,7 +2,8 @@
 crontab -e
 ## add the following line to script
 # block website at 8:30AM from Monday to Friday
-30 8 * * 0-5 pihole --wild --regex zhihu.com 163.com ifanr.com huxiu.com leiphone.com bilibili.com youtube.com twitch.com amazon.com amazon.ca ebay.com ebay.ca taobao.com aliexpress.com dealmoon.ca dealmoon.com smzdm.com canadadz.com
-
+30 8 * * 1-5 PATH="$PATH:/usr/sbin:/usr/local/bin/" pihole --wild --regex zhihu.com ifanr.com huxiu.com leiphone.com bilibili.com youtube.com twitch.com amazon.com amazon.ca ebay.com ebay.ca taobao.com aliexpress.com dealmoon.ca dealmoon.com smzdm.com canadadz.com
+#
 # unblock website at 16:30 from Monday to Friday
-30 16 * * 0-5 pihole -w --nuke
+30 16 * * 1-5 PATH="$PATH:/usr/sbin:/usr/local/bin/" pihole --white-wild zhihu.com ifanr.com huxiu.com leiphone.com bilibili.com youtube.com twitch.com amazon.com amazon.ca ebay.com ebay.ca taobao.com aliexpress.com dealmoon.ca dealmoon.com smzdm.com canadadz.com
+
